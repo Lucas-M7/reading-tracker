1ed9037682dd054501a1d1fccf4c7b5dd3d824bc
Lucas-M7 <Lucas015x@gmail.com>
2025-07-28 15:10:00 -0300

    raw: Migrations adicionada e atualizada após a inclusão dos enums

--- Migrations/20250728180455_BookAndReadingEnum.Designer.cs
+++ Migrations/20250728180455_BookAndReadingEnum.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ReadingTracker.API.Data;
 
@@ -11,9 +12,11 @@ using ReadingTracker.API.Data;
 namespace ReadingTracker.API.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250728180455_BookAndReadingEnum")]
+    partial class BookAndReadingEnum
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
