abab6ca084b767482e121cb76ce181dafb75b5eb
Lucas-M7 <Lucas015x@gmail.com>
2025-08-25 21:00:53 -0300

    feat: removendo a classe user

--- Services/Interfaces/IUserService.cs
+++ Services/Interfaces/IUserService.cs
@@ -1,13 +1,12 @@
 using ReadingTracker.API.Dtos.UserDTO;
+using ReadingTracker.API.Entities.Identity;
 
 namespace ReadingTracker.API.Services.Interfaces;
 
 public interface IUserService
 {
-    public Task<UserReadDTO?> GetByIdAsync(Guid id);
-    Task<IEnumerable<UserReadDTO>> GetAllsAsync();
-
-    Task<UserCreateDTO> CreateAsync(UserCreateDTO createDTO);
-    Task<UserUpdateDTO> UpdateAsync(Guid id, UserUpdateDTO updateDTO);
+    Task<ApplicationUser> RegisterAsync(UserCreateDTO createDTO);
+    Task<bool> UpdateAsync(Guid id, UserUpdateDTO updateDTO);
     Task<bool> DeleteAsync(Guid id);
+    Task<string> LoginAsync(UserLoginDTO loginDTO);
 }
