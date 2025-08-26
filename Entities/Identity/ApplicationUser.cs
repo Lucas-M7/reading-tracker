6061539dee41ab678ed726fb207428486ce3ec56
Lucas-M7 <Lucas015x@gmail.com>
2025-08-25 21:00:16 -0300

    feat: adicionando propriedades de navegação

--- Entities/Identity/ApplicationUser.cs
+++ Entities/Identity/ApplicationUser.cs
@@ -2,7 +2,11 @@ using Microsoft.AspNetCore.Identity;
 
 namespace ReadingTracker.API.Entities.Identity;
 
-public class ApplicationUser : IdentityUser
+public class ApplicationUser : IdentityUser<Guid>
 {
-    
+    public string FullName { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; }
+
+    public ICollection<Book> Books { get; set; } = new List<Book>();
+    public ICollection<Reading> Readings { get; set; } = new List<Reading>();
 }
