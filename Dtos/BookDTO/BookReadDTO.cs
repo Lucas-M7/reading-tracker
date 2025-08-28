99bc0f850063be1dc691b8033a7f05f6a56bb000
Lucas-M7 <Lucas015x@gmail.com>
2025-08-27 21:52:52 -0300

    refactor: refatorando DTOs do Book

--- Dtos/BookDTO/BookReadDTO.cs
+++ Dtos/BookDTO/BookReadDTO.cs
@@ -9,4 +9,20 @@ public class BookReadDTO
     public string Author { get; set; } = string.Empty;
     public Genre Genre { get; set; } = Genre.Other;
     public int TotalPages { get; set; }
+    public DateTime DateAdded { get; set; }
+
+    /// <summary>
+    /// Total de páginas que o usuário já leu deste livro,
+    /// </summary>
+    public int PagesRead { get; set; }
+
+    /// <summary>
+    /// Percentual do progresso da leitura (0 a 100).
+    /// </summary>
+    public double Progress { get; set; }
+
+    /// <summary>
+    /// Indica se o livro já foi completamente lido.
+    /// </summary>
+    public bool IsFinished { get; set; }
 }
--- Dtos/BookDTO/BookUpdateDTO.cs
+++ Dtos/BookDTO/BookUpdateDTO.cs
@@ -1,11 +1,19 @@
+using System.ComponentModel.DataAnnotations;
 using ReadingTracker.API.Enums;
 
 namespace ReadingTracker.API.Dtos.BookDTO;
 
 public class BookUpdateDTO
 {
+    [Required(ErrorMessage = "O título é obrigatório.")]
+    [StringLength(100, ErrorMessage = "O título não pode ter mais de 100 caracteres.")]
     public string Title { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "O autor é obrigatório.")]
+    [StringLength(100, ErrorMessage = "O autor não pode ter mais de 100 caracteres.")]
     public string Author { get; set; } = string.Empty;
     public Genre Genre { get; set; } = Genre.Other;
+
+    [Range(1, int.MaxValue, ErrorMessage = "O número total de páginas deve ser de no mínimo 1.")]
     public int TotalPages { get; set; }
 }
