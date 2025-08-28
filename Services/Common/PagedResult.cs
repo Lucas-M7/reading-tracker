90ea8844de2719454bc062affd9d68434940dc22
Lucas-M7 <Lucas015x@gmail.com>
2025-08-27 21:55:10 -0300

    feat: adicionando classe de paginação de livros

---
+++ Services/Common/PagedResult.cs
@@ -0,0 +1,7 @@
+namespace ReadingTracker.API.Services.Common;
+
+public class PagedResult<T>
+{
+    public IEnumerable<T> Items { get; set; } = new List<T>();
+    public int TotalCount { get; set; }
+}
