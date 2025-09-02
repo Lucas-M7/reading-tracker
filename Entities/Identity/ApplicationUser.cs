dc61b4f812c452d61a093466f0f080267361fe32
Lucas-M7 <Lucas015x@gmail.com>
2025-09-01 22:47:25 -0300

    refactor: modificando o encapsulamento das propriedades e adicionando um construtor para inicialização do objeto

--- Entities/Identity/ApplicationUser.cs
+++ Entities/Identity/ApplicationUser.cs
@@ -5,8 +5,13 @@ namespace ReadingTracker.API.Entities.Identity;
 public class ApplicationUser : IdentityUser<Guid>
 {
     public string FullName { get; set; } = string.Empty;
-    public DateTime CreatedAt { get; set; }
+    public DateTime CreatedAt { get; init; }
 
-    public ICollection<Book> Books { get; set; } = new List<Book>();
-    public ICollection<Reading> Readings { get; set; } = new List<Reading>();
+    public ICollection<Book> Books { get; private set; } = new List<Book>();
+    public ICollection<Reading> Readings { get; private set; } = new List<Reading>();
+
+    public ApplicationUser()
+    {
+        CreatedAt = DateTime.UtcNow;
+    }
 }
