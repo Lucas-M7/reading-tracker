068e5be0ca99181c05634a5da4ce513ca1ed1639
Lucas-M7 <Lucas015x@gmail.com>
2025-09-15 22:01:39 -0300

    refactor: removendo a conversão desnecessária para string  do user.FullName

--- Services/TokenService.cs
+++ Services/TokenService.cs
@@ -22,7 +22,7 @@ public class TokenService : ITokenService
         {
             new Claim(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
             new Claim(JwtRegisteredClaimNames.Email, user.Email),
-            new Claim(JwtRegisteredClaimNames.GivenName, user.FullName.ToString()),
+            new Claim(JwtRegisteredClaimNames.GivenName, user.FullName),
         };
 
         var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
